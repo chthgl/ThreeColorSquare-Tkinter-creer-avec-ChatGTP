a38cc4b34a66f41c1dcf96e3a081d67c3c6b77ca
chthgl <120177312+chthgl@users.noreply.github.com>
2022-12-09 15:44:14 +0100

    Update carrer.py
    C'est plus des carré mais des rond, les commentaire vont revenir.

--- carrer.py
+++ carrer.py
@@ -1,140 +1,116 @@
-# -*- coding: utf-8 -*-
 import tkinter as tk
 from random import randint, choice
-
+import math
 class Square:
     COLORS = ["green", "yellow", "red", "blue", "purple", "orange"]
     def __init__(self, x, y, color):
         self.size = randint(4,8)
+        randomvitesse = randint(1,3)
         self.x = x
         self.y = y
-        self.dx = 1 # déplacement en x
-        self.dy = 1 # déplacement en y
+        self.dx = randomvitesse
+        self.dy = randomvitesse
         self.color = color
-        self.rect = canvas.create_rectangle(self.x, self.y, self.x + self.size, self.y + self.size, fill=self.color)
-        
+        #self.rect = canvas.create_rectangle(self.x, self.y, self.x + self.size, self.y + self.size, fill=self.color)
+        self.rect = canvas.create_oval(self.x, self.y, self.x + self.size, self.y + self.size, fill=self.color)
     def move(self):
-        # Vérifie si le carré touche un bord de la fenêtre
         if self.x + SIZE >= WIDTH or self.x <= 0:
             self.dx *= -1
         if self.y + SIZE >= HEIGHT or self.y <= 0:  
             self.dy *= -1
-
-        # Déplacement du carré
         self.x += self.dx
         self.y += self.dy
         canvas.move(self.rect, self.dx, self.dy)
         canvas.bind("<Motion>", self.on_mouse_move)
         canvas.bind("<Button-1>", self.on_click)
-
     def on_click(self, event):
-        # Récupère les coordonnées de la souris lorsque l'événement "clic" est déclenché
         mouse_x = event.x
         mouse_y = event.y
-
-        # Déclare clicked_square comme étant une variable globale
         global clicked_square
-
-        # Initialise la variable clicked_square à None
         clicked_square = None
-
-        # Pour chaque carré
         for square in squares:
-            # Si la souris est à côté du carré (à une distance inférieure à la moitié de la taille du carré)
             if (abs(square.x - mouse_x) < SIZE and abs(square.y - mouse_y) < SIZE):
-                # On stocke le carré dans une variable globale
                 clicked_square = square
         if clicked_square is not None:
             clicked_square.change_color(choice(Square.COLORS))
-
-
     def on_mouse_move(self, event):
-        # Récupère la position actuelle de la souris
         mouse_x = event.x
         mouse_y = event.y
         self.change_color(choice(Square.COLORS))
-
-        # Pour chaque carré
         for square in squares:
-            # Si la souris est à côté du carré (à une distance inférieure à la moitié de la taille du carré)
-            if (abs(square.x - mouse_x) < SIZE and abs(square.y - mouse_y) < SIZE):
-                # On inverse la direction de déplacement du carré
+            distance = math.sqrt((square.x - mouse_x)**2 + (square.y - mouse_y)**2)
+
+            if distance <= 10:  # Ajout d'une condition pour vérifier la distance
                 square.dx *= -1
                 square.dy *= -1
+            elif (abs(square.x - mouse_x) < SIZE and abs(square.y - mouse_y) < SIZE):
+                square.dx *= -1
+                square.dy *= -1
+    def change_speed(self):
+        for square in squares:
+            self.dx = randint(1, 3) # Modification aléatoire de la vitesse en x
+            self.dy = randint(1, 3) # Modification aléatoire de la vitesse en y
+        
 
 
-
-    # Ajout d'une fonction pour changer la couleur d'un carré
     def change_color(self, color):
         self.color = color
         canvas.itemconfig(self.rect, fill=color)
-
-    # Ajout d'une fonction pour détecter les collisions entre les carrés
+    #Les plus petit prennent la couleur des carré plus gros, 
+    #si le carré plus gros vas plus vite que le carré plus petit ils gardes la même couleur, 
+    #si le carré plus petit vas plus vite que le plus gros le plus gros deviens noir    
     def detect_collision(self, other_square):
-        # Vérifie si les carrés se chevauchent
         if (self.x < other_square.x + SIZE and self.x + SIZE > other_square.x and
         self.y < other_square.y + SIZE and self.y + SIZE > other_square.y):
-            if self.color == other_square.color:
-                self.change_color("black")
-                other_square.change_color("black")
-            
-            # Inverse la direction de déplacement des deux carrés
+            if self.size > other_square.size:
+                if self.dx > other_square.dx and self.dy > other_square.dy:
+                    other_square.change_color(self.color)
+                else:
+                    other_square.change_color("black")
+            elif self.size < other_square.size:
+                if self.dx < other_square.dx and self.dy < other_square.dy:
+                    self.change_color(other_square.color)
+                else:
+                    self.change_color("black")
             self.dx *= -1
             self.dy *= -1
             other_square.dx *= -1
             other_square.dy *= -1
 
-            # Change la couleur des carrés en fonction de leur couleur actuelle
- 
-
-
-# Définition des constantes
-SIZE = 10 # taille des carrés en pixels
+SIZE = 10 
 speedrand = randint(15, 40)
-SPEED = speedrand # vitesse de déplacement en ms
-COLORS = ["green", "yellow", "red"] # couleurs des carrés
-
-# Création de la fenêtre principale
+SPEED = speedrand 
+COLORS = ["green", "yellow", "red"] 
 window = tk.Tk()
 window.title("Carrés")
-
-# Création du canvas
-#WIDTH = 750
-#HEIGHT = 750
 WIDTH = window.winfo_screenwidth()
 HEIGHT = window.winfo_screenheight()
+WIDTH = WIDTH - 100
+HEIGHT =HEIGHT - 100
 canvas = tk.Canvas(window, width=WIDTH, height=HEIGHT)
 canvas.pack()
-
-# Création des carrés
 squares = []
+
 for j in range(65):
-    
     for i in range(3):
         x = randint(0, WIDTH-SIZE)
         y = randint(0, HEIGHT-SIZE)
         color = COLORS[i]
         squares.append(Square(x, y, color))
-
-# Ajout de trois carrés supplémentaires
 squares.append(Square(0, 0, "blue"))
 squares.append(Square(WIDTH-SIZE, 0, "purple"))
 squares.append(Square(0, HEIGHT-SIZE, "orange"))
 
-# Fonction appelée régulièrement pour mettre à jour l'affichage
 def update():
+    
     for square in squares:
         square.move()
 
-        # Détecte les collisions entre les carrés
         for other_square in squares:
             if square != other_square:
                 square.detect_collision(other_square)
-
-    # Appel de la fonction pour mettre à jour l'affichage
+    
+    window.after(1000, square.change_speed)
     window.after(SPEED, update)
-# Appel de la fonction pour mettre à jour l'affichage
 update()
-
-# Affichage de la fenêtre
 window.mainloop()
